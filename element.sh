1f08a83486a1a5c3ae8d93c4947e2c7629525d25
shubham <shubham@gmail.com>
2022-09-26 20:47:58 +0530

    [shubham]- array program

---
+++ element.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -x
+
+index_array=(0 1 2 3 4 5 6 7 8 9)
+echo ${index_array[4]}
---
+++ large.sh
@@ -0,0 +1,11 @@
+#!/bin/bash/ -x
+
+num1=20
+num2=10
+if [ $num1 -ge $num2 ]
+
+then
+        echo "$num2 is greater than or equal to $num1"
+else
+            echo "$num2 is less then $num1"
+fi
